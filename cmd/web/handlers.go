7646586aaae732e910d56d5f2816ad4db8a54317
daiyr bokan <frogice10@gmail.com>
2025-01-30 14:22:28 +0500

    initial handlers & web-server

---
+++ cmd/web/handlers.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("Welcome to the Game Market!"))
+}
+
+func gameShow(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 0 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Showing game with id:%d", id)
+}
+func gameAdd(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Adding a specific game"))
+}
---
+++ cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// creating new router
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/game/show", gameShow)
+	mux.HandleFunc("/game/add", gameAdd)
+
+	// listening on a web-server
+	log.Print("Starting web server on a localhost:4000")
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
